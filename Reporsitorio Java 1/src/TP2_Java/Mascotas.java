92a0db9ec62373020bd35bcfa2bd52ae2e8571a2
unknown <ortelladokaren@gmail.com>
2025-11-19 15:57:03 -0300

    Trabajo Práctico 2 - ejercicios

---
+++ Reporsitorio Java 1/src/TP2_Java/Mascotas.java
@@ -0,0 +1,20 @@
+
+package TP2_Java;
+
+
+public class Mascotas {
+    private String nombre="Elias",especie="Pastor Aleman";
+    private int edad=7;
+    
+    void mostrarInfo(){
+        System.out.println("Nombre: "+nombre+"\nEspecie: "+especie+"\nEdad: "+edad);
+        
+    }
+    void cumplirAnios (int anios) {
+        if (anios>0);
+            edad+=anios;
+        
+    }  
+    
+    
+}
